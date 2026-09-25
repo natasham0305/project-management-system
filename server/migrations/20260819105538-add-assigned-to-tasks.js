3e1e97eeca877d257c7477e71ea3b5917518ca8d
natasham0305 <natashamalviya0305@email.com>
2026-09-25 12:54:46 +0530

    fix task migration table name

--- server/migrations/20260819105538-add-assigned-to-tasks.js
+++ server/migrations/20260819105538-add-assigned-to-tasks.js
@@ -1,9 +1,10 @@
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("tasks", "assigned_to", {
+    await queryInterface.addColumn("Tasks", "assigned_to", {
       type: Sequelize.INTEGER,
       allowNull: true,
       references: {
@@ -16,6 +17,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("tasks", "assigned_to");
+    await queryInterface.removeColumn("Tasks", "assigned_to");
   },
 };
