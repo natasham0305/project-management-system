7cb9e3bef5a605e3dec71f29c969d5d68d7d54fa
natasham0305 <natashamalviya0305@email.com>
2026-09-25 13:28:35 +0530

    typo error fixed

--- server/migrations/20260824114656-create-project-message.js
+++ server/migrations/20260824114656-create-project-message.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "projects",
+          model: "Projects",
           key: "id",
         },
         onUpdate: "CASCADE",
