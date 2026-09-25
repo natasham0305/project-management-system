e799a5cd61d800d6e80c3e90427371f50b34b7ce
natasham0305 <natashamalviya0305@email.com>
2026-09-25 12:23:00 +0530

    fix production cors

--- server/server.js
+++ server/server.js
@@ -19,13 +19,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://project-management-system-theta-tan.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
 app.set("io", io);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
